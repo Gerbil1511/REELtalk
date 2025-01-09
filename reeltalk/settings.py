9adad31690d42bc978fefdfe19f647e895d7e0f3
Geraldine Edwards <gerbil1511@gmail.com>
2025-01-09 13:41:38 +0000

    Add Procfile for deployment and update ALLOWED_HOSTS

--- reeltalk/settings.py
+++ reeltalk/settings.py
@@ -35,6 +35,8 @@ DEBUG = False
 ALLOWED_HOSTS = [
                 '127.0.0.1',
                 'localhost',
+                '.herokuapp.com',
+                '.codeanyapp.com',
 ]
 
 # Application definition
