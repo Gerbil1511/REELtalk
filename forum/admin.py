f2754835bdcc67dca2894300065742ae8360378a
Geraldine Edwards <gerbil1511@gmail.com>
2025-01-10 13:51:42 +0000

    Register ForumPost and Movie models in the admin interface with custom display and search options

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ForumPost
 
 # Register your models here.
+@admin.register(ForumPost)
+class ForumPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'movie', 'created_at', 'updated_at', 'total_upvotes', 'total_downvotes')
+    search_fields = ('title', 'content', 'author__username', 'movie__title')
